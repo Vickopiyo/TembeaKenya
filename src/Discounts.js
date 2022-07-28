7f6a94b12ac1714b1a27a862d9e2175435b2457d
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-28 12:40:45 +0300

    Fixes Delete button

--- src/Discounts.js
+++ src/Discounts.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Discounts = () => {
   return (
     <div>
-        <h3>Tembea Kenya appreciates it's loyal clients through discounts,vouchers and awards </h3>
+        <h3>Tembea Kenya appreciates it's loyal clients through discounts,vouchers and cash awards </h3>
             <ul>
                 <li>25% discount on your third booking with us</li>
                 <li>We give cash awards to our clients who refer other clients</li>
