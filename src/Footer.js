c523113fec99ae98eb611c59f15fd71ac9ccc79a
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-28 11:05:33 +0300

    Adds footer component

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,9 @@ const Footer = () => {
   return (
     <footer>
      <h4>Copyrights 2022.All Rights are reserved.</h4>
-     <div>
+      <div>
         
-     </div>
-
+      </div>
     </footer>
   )
 }
