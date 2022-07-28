9b9bf7eb9eb51ed2e376f1b1a2a87676e83abc6a
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-28 12:39:41 +0300

    styles footer

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-        <h1>Tembea <span>Kenya</span> </h1>
-          <ul>
+         <h1>Tembea <span>Kenya</span> </h1>
+          <ul className='route-links'>
             <li>Our Tours</li>
             <li>Offers and Discount</li>
             <li>Donate </li>
