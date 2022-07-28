f08dc90d2c18c251b657d22418403e69082e1114
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-28 12:17:07 +0300

    Adds tours components

--- src/Donate.js
+++ src/Donate.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Donate = () => {
   return (
     <div className='donate'>
-      <h2>Thank you for landing here!</h2>
+      <h2>Thank you for being here here!</h2>
        <h3>Feel free to support us via :</h3>
      <ul>
         <li>MPESA : 0701341969</li>
