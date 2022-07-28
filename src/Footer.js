4c7cab51c0ce28a1c03277f5a471babcebd62530
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-28 11:06:52 +0300

    Adds licenses

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,7 @@ const Footer = () => {
   return (
     <footer>
      <h4>Copyrights 2022.All Rights are reserved.</h4>
-      <div>
-        
-      </div>
+     <p>Made by Telekom technologies</p>
     </footer>
   )
 }
